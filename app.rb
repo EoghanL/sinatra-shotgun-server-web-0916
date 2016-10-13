fb7d49973fb21ae8b7af16b65c2399c80d5ed822
github username <github email address>
2016-10-12 21:33:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! You can even interpolate: 348 squared is - #{348 ** 2} and you can add on reload with shotgun"
   end
 
 end
